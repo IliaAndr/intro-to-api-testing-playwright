7538167b55279ece13b80c051446ff58ccf9407e
iliaandrianov <ilia.i.andrianov@gmail.com>
2025-06-09 20:01:05 +0300

    added new tests

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -31,3 +31,56 @@ test('post order with correct data should receive code 201', async ({ request })
   console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
 })
+
+test('get order with 0 id should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/0')
+  // Log the response status, body and headers
+  // console.log('response body:', await response.json()) // не хотим выводить в консоль
+  // console.log('response headers:', response.headers()) // не хотим выводить в консоль
+  // Check if the response status is 400
+  expect(response.status()).toBe(400)
+})
+
+test('get order with 11 id should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/11')
+  // Log the response status, body and headers
+  // console.log('response body:', await response.json()) // не хотим выводить в консоль
+  // console.log('response headers:', response.headers()) // не хотим выводить в консоль
+  // Check if the response status is 400
+  expect(response.status()).toBe(400)
+})
+
+test('get order with null id should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/')
+  // Log the response status, body and headers
+  // console.log('response body:', await response.json()) // не хотим выводить в консоль
+  // console.log('response headers:', response.headers()) // не хотим выводить в консоль
+  // Check if the response status is 500
+  expect(response.status()).toBe(500)
+})
+
+test('get order with string id should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/test')
+  // Log the response status, body and headers
+  console.log('response body:', await response.json()) // не хотим выводить в консоль
+  console.log('response headers:', response.headers()) // не хотим выводить в консоль
+  // Check if the response status is 400
+  expect(response.status()).toBe(400)
+})
+
+test('post order with incorrect data should receive code 415', async ({ request }) => {
+  // prepare request body
+
+  // Send a POST request to the server
+  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+    data: 'test',
+  })
+  // Log the response status and body
+  console.log('response status:', response.status())
+  console.log('response body:', await response.json())
+  expect(response.status()).toBe(StatusCodes.UNSUPPORTED_MEDIA_TYPE)
+})
