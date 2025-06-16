d92b4939ab87e6043bf564de12f08da7f8fe2d9f
iliaandrianov <ilia.i.andrianov@gmail.com>
2025-06-16 19:34:00 +0300

    added new tests lesson 10 (prettier)

--- tests/dto/order-dto.ts
+++ tests/dto/order-dto.ts
@@ -1,10 +1,10 @@
 export class OrderDto {
-  status: string;
-  courierId: number;
-  customerName: string;
-  customerPhone: string;
-  comment: string;
-  id: number;
+  status: string
+  courierId: number
+  customerName: string
+  customerPhone: string
+  comment: string
+  id: number
 
   private constructor(
     customerName: string,
@@ -12,24 +12,23 @@ export class OrderDto {
     comment: string,
     id: number,
     status: string,
-    courierId: number
+    courierId: number,
   ) {
-    this.customerName = customerName;
-    this.customerPhone = customerPhone;
-    this.comment = comment;
-    this.id = id;
-    this.status = status;
-    this.courierId = courierId;
+    this.customerName = customerName
+    this.customerPhone = customerPhone
+    this.comment = comment
+    this.id = id
+    this.status = status
+    this.courierId = courierId
   }
   static createOrderWithRandomData(): OrderDto {
     return new OrderDto(
       'John Doe',
       '+123345678',
       'test comment',
-      Math.floor(Math.random()*100),
+      Math.floor(Math.random() * 100),
       'OPEN',
-      Math.floor(Math.random()*100)
+      Math.floor(Math.random() * 100),
     )
   }
 }
-
